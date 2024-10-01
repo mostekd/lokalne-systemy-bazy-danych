0df432219e35cc16ee11de8db01bb94dd842754e
Dawid Mostowski <dawid-mostowski@o2.pl>
2024-10-01 19:02:43 +0200

    update

--- Klasa 4/klucze_opce-i-uzytkownicy.sql
+++ Klasa 4/klucze_opce-i-uzytkownicy.sql
@@ -37,4 +37,40 @@ ALTER TABLE Dostawcy_do_zamowienia
     ADD FOREIGN KEY (id_dostawca) REFERENCES Dostawcy(id_dostawcy)
 ;
 
-create user Administrator 
+CREATE USER 'administrator'@'localhost' IDENTIFIED BY 'admin';
+GRANT SELECT, INSERT, UPDATE, DELETE ON sklep.zamówienia TO 'administrator'@'localhost';
+GRANT SELECT, INSERT, UPDATE, DELETE ON sklep.typy_produktu TO 'administrator'@'localhost';
+GRANT SELECT, INSERT, UPDATE, DELETE ON sklep.typy_platnosci TO 'administrator'@'localhost';
+GRANT SELECT, INSERT, UPDATE, DELETE ON sklep.sklepy TO 'administrator'@'localhost';
+GRANT SELECT, INSERT, UPDATE, DELETE ON sklep.typy_dostawy TO 'administrator'@'localhost';
+GRANT SELECT, INSERT, UPDATE, DELETE ON sklep.stanowiska TO 'administrator'@'localhost';
+GRANT SELECT, INSERT, UPDATE, DELETE ON sklep.produkty TO 'administrator'@'localhost';
+GRANT SELECT, INSERT, UPDATE, DELETE ON sklep.lokalizacja_do_produkt TO 'administrator'@'localhost';
+GRANT SELECT, INSERT, UPDATE, DELETE ON sklep.pracownicy TO 'administrator'@'localhost';
+GRANT SELECT, INSERT, UPDATE, DELETE ON sklep.lokaliazcje TO 'administrator'@'localhost';
+GRANT SELECT, INSERT, UPDATE, DELETE ON sklep.dostawcy_do_zamowienia TO 'administrator'@'localhost';
+GRANT SELECT, INSERT, UPDATE, DELETE ON sklep.dostawcy TO 'administrator'@'localhost';
+FLUSH PRIVILEGES;
+
+CREATE USER 'system'@'localhost' IDENTIFIED BY 'system';
+GRANT SELECT, DELETE ON sklep.klienci TO 'system'@'localhost';
+FLUSH PRIVILEGES;
+
+CREATE USER 'klient'@'localhost' IDENTIFIED BY 'klient';
+GRANT UPDATE ON sklep.klienci TO 'klient'@'localhost';
+GRANT SELECT, INSERT, UPDATE ON sklep.klienci TO 'klient'@'localhost';
+GRANT SELECT ON sklep.produkty TO 'administrator'@'localhost';
+GRANT SELECT ON sklep.sklepy TO 'administrator'@'localhost';
+FLUSH PRIVILEGES;
+
+CREATE USER 'pracownik'@'localhost' IDENTIFIED BY 'pracownik';
+GRANT SELECT, INSERT, UPDATE ON  sklep.dostawcy TO 'pracownik'@'localhost';
+GRANT SELECT, INSERT, UPDATE ON  sklep.dostawcy_do_zamowienia TO 'pracownik'@'localhost';
+GRANT SELECT ON  sklep.klienci TO 'pracownik'@'localhost';
+GRANT SELECT ON  sklep.magazyny TO 'pracownik'@'localhost';
+GRANT SELECT ON  sklep.pracownicy TO 'pracownik'@'localhost';
+GRANT SELECT, INSERT, UPDATE ON  sklep.lokalizacja_do_produkt TO 'pracownik'@'localhost';
+GRANT SELECT, INSERT, UPDATE ON  sklep.produkty TO 'pracownik'@'localhost';
+GRANT SELECT  ON  sklep.sklepy TO 'pracownik'@'localhost';
+GRANT SELECT, UPDATE ON  sklep.zamowienia TO 'pracownik'@'localhost';
+FLUSH PRIVILEGES;
