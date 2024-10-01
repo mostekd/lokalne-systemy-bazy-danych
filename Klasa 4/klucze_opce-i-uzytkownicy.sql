974c64d5b75010e325a4a26f3024c28e861e9b19
Dawid Mostowski <dawid-mostowski@o2.pl>
2024-10-01 19:35:40 +0200

    update

--- Klasa 4/klucze_opce-i-uzytkownicy.sql
+++ Klasa 4/klucze_opce-i-uzytkownicy.sql
@@ -20,14 +20,14 @@ ALTER TABLE Magazyny
     ADD FOREIGN KEY (id_lokalizacja) REFERENCES Lokalizacje(id_lokalizacja)
 ;
 ALTER TABLE Sklepy
-    ADD FOREIGN KEY (id_pracownik_menager) REFERENCES Pracownicy(id_pracownik),
+    ADD FOREIGN KEY (id_pracownik-sklep_menager) REFERENCES Pracownicy(id_pracownik-sklep),
     ADD FOREIGN KEY (id_lokalizacja) REFERENCES Lokalizacje(id_lokalizacja)
 ;
 ALTER TABLE Zamowienia
-    ADD FOREIGN KEY (id_klienta) REFERENCES Klienci(id_klient),
+    ADD FOREIGN KEY (id_klient-sklepa) REFERENCES Klienci(id_klient-sklep),
     ADD FOREIGN KEY (id_lokalizacja_produktu) REFERENCES Lokalizacje(id_lokalizacja),
     ADD FOREIGN KEY (id_lokalizacja_odbioru) REFERENCES Lokalizacje(id_lokalizacja),
-    ADD FOREIGN KEY (id_pracownika) REFERENCES Pracownicy(id_pracownik),
+    ADD FOREIGN KEY (id_pracownik-sklepa) REFERENCES Pracownicy(id_pracownik-sklep),
     ADD FOREIGN KEY (id_typ_platnosci) REFERENCES Typy_platnosci(id_typ_platnosci),
     ADD FOREIGN KEY (id_produkt) REFERENCES Produkty(id_produktu),
     ADD FOREIGN KEY (id_typ_dostawy) REFERENCES typy_dostawy(id_typ_dostawy)
@@ -37,40 +37,40 @@ ALTER TABLE Dostawcy_do_zamowienia
     ADD FOREIGN KEY (id_dostawca) REFERENCES Dostawcy(id_dostawcy)
 ;
 
-CREATE USER 'administrator'@'localhost' IDENTIFIED BY 'admin';
-GRANT SELECT, INSERT, UPDATE, DELETE ON sklep.zamowienia TO 'administrator'@'localhost';
-GRANT SELECT, INSERT, UPDATE, DELETE ON sklep.typy_produktu TO 'administrator'@'localhost';
-GRANT SELECT, INSERT, UPDATE, DELETE ON sklep.typy_platnosci TO 'administrator'@'localhost';
-GRANT SELECT, INSERT, UPDATE, DELETE ON sklep.sklepy TO 'administrator'@'localhost';
-GRANT SELECT, INSERT, UPDATE, DELETE ON sklep.typy_dostawy TO 'administrator'@'localhost';
-GRANT SELECT, INSERT, UPDATE, DELETE ON sklep.stanowiska TO 'administrator'@'localhost';
-GRANT SELECT, INSERT, UPDATE, DELETE ON sklep.produkty TO 'administrator'@'localhost';
-GRANT SELECT, INSERT, UPDATE, DELETE ON sklep.lokalizacja_do_produkt TO 'administrator'@'localhost';
-GRANT SELECT, INSERT, UPDATE, DELETE ON sklep.pracownicy TO 'administrator'@'localhost';
-GRANT SELECT, INSERT, UPDATE, DELETE ON sklep.lokaliazcje TO 'administrator'@'localhost';
-GRANT SELECT, INSERT, UPDATE, DELETE ON sklep.dostawcy_do_zamowienia TO 'administrator'@'localhost';
-GRANT SELECT, INSERT, UPDATE, DELETE ON sklep.dostawcy TO 'administrator'@'localhost';
+CREATE USER 'administrator-sklep-sklep'@'localhost' IDENTIFIED BY 'admin';
+GRANT SELECT, INSERT, UPDATE, DELETE ON sklep.zamowienia TO 'administrator-sklep'@'localhost';
+GRANT SELECT, INSERT, UPDATE, DELETE ON sklep.typy_produktu TO 'administrator-sklep'@'localhost';
+GRANT SELECT, INSERT, UPDATE, DELETE ON sklep.typy_platnosci TO 'administrator-sklep'@'localhost';
+GRANT SELECT, INSERT, UPDATE, DELETE ON sklep.sklepy TO 'administrator-sklep'@'localhost';
+GRANT SELECT, INSERT, UPDATE, DELETE ON sklep.typy_dostawy TO 'administrator-sklep'@'localhost';
+GRANT SELECT, INSERT, UPDATE, DELETE ON sklep.stanowiska TO 'administrator-sklep'@'localhost';
+GRANT SELECT, INSERT, UPDATE, DELETE ON sklep.produkty TO 'administrator-sklep'@'localhost';
+GRANT SELECT, INSERT, UPDATE, DELETE ON sklep.lokalizacja_do_produkt TO 'administrator-sklep'@'localhost';
+GRANT SELECT, INSERT, UPDATE, DELETE ON sklep.pracownicy TO 'administrator-sklep'@'localhost';
+GRANT SELECT, INSERT, UPDATE, DELETE ON sklep.lokaliazcje TO 'administrator-sklep'@'localhost';
+GRANT SELECT, INSERT, UPDATE, DELETE ON sklep.dostawcy_do_zamowienia TO 'administrator-sklep'@'localhost';
+GRANT SELECT, INSERT, UPDATE, DELETE ON sklep.dostawcy TO 'administrator-sklep'@'localhost';
 FLUSH PRIVILEGES;
 
-CREATE USER 'system'@'localhost' IDENTIFIED BY 'system';
-GRANT SELECT, DELETE ON sklep.klienci TO 'system'@'localhost';
+CREATE USER 'system-sklep'@'localhost' IDENTIFIED BY 'system';
+GRANT SELECT, DELETE ON sklep.klienci TO 'system-sklep'@'localhost';
 FLUSH PRIVILEGES;
 
-CREATE USER 'klient'@'localhost' IDENTIFIED BY 'klient';
-GRANT UPDATE ON sklep.klienci TO 'klient'@'localhost';
-GRANT SELECT, INSERT, UPDATE ON sklep.klienci TO 'klient'@'localhost';
-GRANT SELECT ON sklep.produkty TO 'administrator'@'localhost';
-GRANT SELECT ON sklep.sklepy TO 'administrator'@'localhost';
+CREATE USER 'klient-sklep'@'localhost' IDENTIFIED BY 'klient';
+GRANT UPDATE ON sklep.klienci TO 'klient-sklep'@'localhost';
+GRANT SELECT, INSERT, UPDATE ON sklep.klienci TO 'klient-sklep'@'localhost';
+GRANT SELECT ON sklep.produkty TO 'klient-sklep'@'localhost';
+GRANT SELECT ON sklep.sklepy TO 'klient-sklep'@'localhost';
 FLUSH PRIVILEGES;
 
-CREATE USER 'pracownik'@'localhost' IDENTIFIED BY 'pracownik';
-GRANT SELECT, INSERT, UPDATE ON  sklep.dostawcy TO 'pracownik'@'localhost';
-GRANT SELECT, INSERT, UPDATE ON  sklep.dostawcy_do_zamowienia TO 'pracownik'@'localhost';
-GRANT SELECT ON  sklep.klienci TO 'pracownik'@'localhost';
-GRANT SELECT ON  sklep.magazyny TO 'pracownik'@'localhost';
-GRANT SELECT ON  sklep.pracownicy TO 'pracownik'@'localhost';
-GRANT SELECT, INSERT, UPDATE ON  sklep.lokalizacja_do_produkt TO 'pracownik'@'localhost';
-GRANT SELECT, INSERT, UPDATE ON  sklep.produkty TO 'pracownik'@'localhost';
-GRANT SELECT  ON  sklep.sklepy TO 'pracownik'@'localhost';
-GRANT SELECT, UPDATE ON  sklep.zamowienia TO 'pracownik'@'localhost';
+CREATE USER 'pracownik-sklep'@'localhost' IDENTIFIED BY 'pracownik';
+GRANT SELECT, INSERT, UPDATE ON  sklep.dostawcy TO 'pracownik-sklep'@'localhost';
+GRANT SELECT, INSERT, UPDATE ON  sklep.dostawcy_do_zamowienia TO 'pracownik-sklep'@'localhost';
+GRANT SELECT ON  sklep.klienci TO 'pracownik-sklep'@'localhost';
+GRANT SELECT ON  sklep.magazyny TO 'pracownik-sklep'@'localhost';
+GRANT SELECT ON  sklep.pracownicy TO 'pracownik-sklep'@'localhost';
+GRANT SELECT, INSERT, UPDATE ON  sklep.lokalizacja_do_produkt TO 'pracownik-sklep'@'localhost';
+GRANT SELECT, INSERT, UPDATE ON  sklep.produkty TO 'pracownik-sklep'@'localhost';
+GRANT SELECT  ON  sklep.sklepy TO 'pracownik-sklep'@'localhost';
+GRANT SELECT, UPDATE ON  sklep.zamowienia TO 'pracownik-sklep'@'localhost';
 FLUSH PRIVILEGES;
