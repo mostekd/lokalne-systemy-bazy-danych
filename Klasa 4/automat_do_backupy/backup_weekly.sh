b021df0574c9360bcc7a1e1b6e22e888d779ee9e
mostekd <dawid-mostowski@o2.pl>
2024-11-26 22:28:38 +0100

    update

--- Klasa 4/automat_do_backupy/backup_weekly.sh
+++ Klasa 4/automat_do_backupy/backup_weekly.sh
@@ -7,10 +7,8 @@ BACKUP_DIR="/opt/lampp/htdocs/szkola/lokalne-systemy-bazy-danych/Klasa 4/backup/
 DATE=$(date +"%Y%m%d_%H%M%S")
 BACKUP_FILE="${BACKUP_DIR}/backup_${DB_NAME}_${DATE}.sql"
 
-# Tworzenie backupu
 mysqldump -u $DB_USER -p$DB_PASSWORD $DB_NAME > $BACKUP_FILE
 
-# Weryfikacja
 if [ $? -eq 0 ]; then
     echo "Backup został pomyślnie wykonany: $BACKUP_FILE"
 else
