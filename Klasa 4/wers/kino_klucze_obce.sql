a9bd3cbbf511306c22b4df55c5733a948f3666ef
mostekd <dawid-mostowski@o2.pl>
2024-11-26 21:25:08 +0100

    Update kino_klucze_obce.sql

--- Klasa 4/wers/kino_klucze_obce.sql
+++ Klasa 4/wers/kino_klucze_obce.sql
@@ -31,18 +31,29 @@ create user 'administrator_kino'@'localhost' identified by 'admin_password';
 create user 'system_kino'@'localhost' identified by 'system_password';
 create user 'pracownik_kino'@'localhost' identified by 'pracownik_password';
 
-grant select on Kino.Klienci to 'klient_kino'@'localhost';
+grant select, delete on Kino.Klienci to 'klient_kino'@'localhost';
 grant select on Kino.Filmy to 'klient_kino'@'localhost';
-grant select, insert on Kino.Zamowienia to 'klient_kino'@'localhost';
+grant select, insert, delete on Kino.Zamowienia to 'klient_kino'@'localhost';
+grant select on Kino.Lokalizacja to 'klient_kino'@'localhost';
+grant select on Kino.Rodzaje_filmu to 'klient_kino'@'localhost';
+grant select on Kino.Rezyserzy to 'klient_kino'@'localhost';
+grant select on Kino.Aktorzy to 'klient_kino'@'localhost';
+grant select on Kino.Kina to 'klient_kino'@'localhost';
 
 grant all privileges on Kino.* to 'administrator_kino'@'localhost';
 
 grant select, insert, update on Kino.Seanse to 'system_kino'@'localhost';
 grant select, insert, update on Kino.Oferta to 'system_kino'@'localhost';
+grant select on Kino.Zamowienia to 'system_kino'@'localhost';
+grant select on Kino.Filmy to 'system_kino'@'localhost';
 
-grant select on Kino.Pracownicy to 'pracownik_kino'@'localhost';
+grant select, delete on Kino.Pracownicy to 'pracownik_kino'@'localhost';
 grant select on Kino.Lokalizacja to 'pracownik_kino'@'localhost';
-grant select, update on Kino.Stanowiska to 'pracownik_kino'@'localhost';
+grant select on Kino.Stanowiska to 'pracownik_kino'@'localhost';
+grant select on Kino.Rodzaje_filmu to 'pracownik_kino'@'localhost';
+grant select on Kino.Rezyserzy to 'pracownik_kino'@'localhost';
+grant select on Kino.Aktorzy to 'pracownik_kino'@'localhost';
+grant select on Kino.Kina to 'pracownik_kino'@'localhost';
 grant select, insert, update on Kino.Seanse to 'pracownik_kino'@'localhost';
 
 flush privileges;
