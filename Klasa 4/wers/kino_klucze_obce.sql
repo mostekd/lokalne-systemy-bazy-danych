2dabbbab3e62474839793d20c1f165283e0881cc
mostekd <dawid-mostowski@o2.pl>
2024-11-26 20:56:43 +0100

    Update kino_klucze_obce.sql

--- Klasa 4/wers/kino_klucze_obce.sql
+++ Klasa 4/wers/kino_klucze_obce.sql
@@ -1,27 +1,48 @@
 use Kino;
 
--- Dodanie kluczy obcych do tabeli Filmy
 alter table Filmy 
     add foreign key (id_rodzaj_filmu) references Rodzaje_filmu(id_rodzaj_filmu),
     add foreign key (id_rezyser) references Rezyserzy(id_rezyser),
     add foreign key (id_aktorzy) references Aktorzy(id_aktor);
 
--- Dodanie kluczy obcych do tabeli Pracownicy
 alter table Pracownicy 
     add foreign key (id_stanowisko) references Stanowiska(id_stanowisko),
     add foreign key (id_lokalizacja_pracy) references Lokalizacja(id_lokalizacja);
 
--- Dodanie kluczy obcych do tabeli Seanse
 alter table Seanse 
     add foreign key (id_film) references Filmy(id_film);
 
--- Dodanie kluczy obcych do tabeli Zamowienia
 alter table Zamowienia 
     add foreign key (id_klient) references Klienci(id_klient),
     add foreign key (id_film) references Filmy(id_film),
     add foreign key (id_kina) references Kina(id_kina);
 
--- Dodanie kluczy obcych do tabeli Kina
 alter table Kina 
     add foreign key (id_lokalizacja) references Lokalizacja(id_lokalizacja),
     add foreign key (id_pracownik_manager) references Pracownicy(id_pracownik);
+
+drop user if exists 'klient_kino'@'localhost';
+drop user if exists 'administrator_kino'@'localhost';
+drop user if exists 'system_kino'@'localhost';
+drop user if exists 'pracownik_kino'@'localhost';
+
+create user 'klient_kino'@'localhost' identified by 'klient_password';
+create user 'administrator_kino'@'localhost' identified by 'admin_password';
+create user 'system_kino'@'localhost' identified by 'system_password';
+create user 'pracownik_kino'@'localhost' identified by 'pracownik_password';
+
+grant select on Kino.Klienci to 'klient'@'localhost';
+grant select on Kino.Filmy to 'klient'@'localhost';
+grant select, insert on Kino.Zamowienia to 'klient'@'localhost';
+
+grant all privileges on Kino.* to 'administrator'@'localhost';
+
+grant select, insert, update on Kino.Seanse to 'system'@'localhost';
+grant select, insert, update on Kino.Oferta to 'system'@'localhost';
+
+grant select on Kino.Pracownicy to 'pracownik'@'localhost';
+grant select on Kino.Lokalizacja to 'pracownik'@'localhost';
+grant select, update on Kino.Stanowiska to 'pracownik'@'localhost';
+grant select, insert, update on Kino.Seanse to 'pracownik'@'localhost';
+
+flush privileges;
