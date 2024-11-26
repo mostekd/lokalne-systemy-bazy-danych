ff280c711efbe1afe17b08bebc11eda38fec5c67
mostekd <dawid-mostowski@o2.pl>
2024-11-26 22:25:37 +0100

    update

---
+++ Klasa 4/automat_do_backupy/backup_weekly.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+DB_NAME="Sklep"
+DB_USER="administrator_sklep"
+DB_PASSWORD="admin"
+BACKUP_DIR="/path/to/backup/directory"
+DATE=$(date +"%Y%m%d_%H%M%S")
+BACKUP_FILE="${BACKUP_DIR}/backup_${DB_NAME}_${DATE}.sql"
+
+# Tworzenie backupu
+mysqldump -u $DB_USER -p$DB_PASSWORD $DB_NAME > $BACKUP_FILE
+
+# Weryfikacja
+if [ $? -eq 0 ]; then
+    echo "Backup został pomyślnie wykonany: $BACKUP_FILE"
+else
+    echo "Błąd podczas wykonywania backupu!"
+fi
+
+
+
+# ustawienie:
+
+# crontab -e
+# 0 3 * * 0 /path/to/backup_weekly.sh
